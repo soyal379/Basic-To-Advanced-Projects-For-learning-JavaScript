3d192512d322d4f63a5bee8e24c9d4a18007232b
SOYAL <mahmoodsaifi379@gmail.com>
2026-03-02 23:16:38 +0530

    add toolbar and text input functionality; implement styling for text editor features

---
+++ Text-Editor-With-JavaScript/script.js
@@ -0,0 +1,73 @@
+let optionBtn = document.querySelectorAll(".option-btn")
+let formatBtn = document.querySelectorAll(".format")
+let advOptionBtn = document.querySelectorAll(".adv-option-btn")
+let alingBtn = document.querySelectorAll(".align")
+let fontName = document.getElementById("fontName")
+let fontSize = document.getElementById("fontSize")
+let writingArea = document.getElementById("text-input")
+let linkBtn = document.getElementById("createLink")
+let spacingBtn = document.querySelectorAll(".spacing")
+let ScriptBtn = document.querySelectorAll(".script")
+
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "Cursive"
+]
+
+const initializer = () => {
+    highlighter(alingBtn, true)
+    highlighter(spacingBtn, true)
+    highlighter(ScriptBtn, true)
+    highlighter(formatBtn, false)
+
+    fontList.map((value)=>{
+        let option = document.createElement("option")
+        option.value = value
+        option.innerHTML = value
+        fontName.appendChild(option)
+    })
+
+    for (let i = 1; i < 7; i++) {
+        let option = document.createElement("option")
+        option.value = i
+        option.innerHTML = i
+        fontSize.appendChild(option)
+    }
+
+    fontSize.value = 3
+}
+
+
+const highlighter = (className, needsRemoval)=>{
+    className.forEach((button)=>{
+        button.addEventListener("click", ()=>{
+            if(needsRemoval){
+                let alreadyActive = false
+
+                if(button.classList.contains("active")){
+                    alreadyActive = true
+                }
+
+                highlighterRemover(className)
+                if(!alreadyActive){
+                    button.classList.add("active")
+                }
+            } else{
+                button.classList.toggle("active")
+            }
+        })
+    })
+}
+
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active")
+    })
+}
+
+window.onload = initializer
